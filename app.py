62e8dec4d7b1e23971b152f7ece7d60c171ec143
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-06-21 00:31:10 +0900

    Edit login and sign up

--- app.py
+++ app.py
@@ -21,10 +21,12 @@ app = Flask(__name__)
 # .env
 load_dotenv()
 DB_URL = os.environ.get('DB_URL')
+# SECRET_KEY도 .env에 추가
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 client = MongoClient(DB_URL, tlsCAFile=certifi.where())
 db = client.dbtooshorts
 
-SECRET_KEY = 'SPARTA'
 #################################
 ##  HTML을 주는 부분             ##
 #################################
@@ -51,34 +53,37 @@ def login():
 def register():
     return render_template('register.html')
 
-
 #################################
 ##  로그인을 위한 API            ##
 #################################
-
 # [회원가입 API]
 # id, pw, nickname을 받아서, mongoDB에 저장합니다.
 # 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
 @app.route('/api/register', methods=['POST'])
 def api_register():
     id_receive = request.form['id_give']
+    nickname_receive = request.form['nickname_give']
+    pw_receive = request.form['pw_give']
+    checked_pw_receive = request.form['checked_pw_give']
+
     # 예외처리1: Id중복
     checked_id = db.user.find_one({'id': id_receive})
     if checked_id is not None:
-        return jsonify({'result': '이미 존재하는 ID입니다.'})
-    # 예외처리2: PW불일치
-    
-    # 예외처리3: Username중복
-        
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
+        return jsonify({'msg': '이미 존재하는 ID입니다.'})
 
+    # 예외처리2: PW불일치
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    checked_pw_hash = hashlib.sha256(checked_pw_receive.encode('utf-8')).hexdigest()
+    if pw_hash!=checked_pw_hash:
+        return jsonify({'msg': 'PW가 일치하지 않습니다.'})
+
+    # 예외처리3: Username중복
+    checked_nickname = db.user.find_one({'id': nickname_receive})
+    if checked_nickname is not None:
+        return jsonify({'msg': '이미 존재하는 Username 입니다.'})
 
     db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
     return jsonify({'result': 'success'})
-
 
 # [로그인 API]
 # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
@@ -92,24 +97,22 @@ def api_login():
 
     # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
     result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
-        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
-        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
-        payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
+    if result is None:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+        
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+    # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+    # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+    # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+    payload = {
+        'id': id_receive,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+    # token을 줍니다.
+    return jsonify({'result': 'success', 'token': token})
 
 
 # [유저 정보 확인 API]
