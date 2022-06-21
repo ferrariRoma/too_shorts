42d11bb8dff37e8d1a8b2afcbf6349f9b13cfc8d
ju-ei8ht <qkr8125@gmail.com>
2022-06-21 16:11:46 +0900

    post db list 추가, 로그인 상태값 넘김(임의)

--- app.py
+++ app.py
@@ -26,15 +26,18 @@ db = client.dbtooshorts
 # home handler
 @app.route('/')
 def home():
+    posts = list(db.postings.find({}, {'_id': False}))
+    return render_template('index.html', posts=posts, state='logout')
+
     # 토큰이 있을 때 nickname을 넘겨줌
     try:
         token_receive = request.cookies.get('mytoken')
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', nickname=user_info["nick"])
+        return render_template('index.html', nickname=user_info["nick"], state='login')
     # 토큰이 없을 때 그냥 index.html렌더링
     except jwt.exceptions.DecodeError:
-        return render_template('index.html')
+        return render_template('index.html', state='logout')
 
 # login page rendering
 @app.route('/login')
